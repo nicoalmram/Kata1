93df05697b5f606e5d969bcbdf17b1c87cc112f6
Nicolás <55482616+nicoalmram@users.noreply.github.com>
2020-01-17 11:44:18 +0000

    Actualización Kata1/Person.java

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -5,18 +5,40 @@
  */
 package kata1;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author Usuario
  */
 public class Person {
-    private String name;
-    private int age;
-    private int birthday;
-    
-    public String getName(String name) {
-        return this.name = name;
+
+    /**
+     * @param args the command line arguments
+     */
+    private final String name;
+    private final Calendar birthday;
+    private final long MILLISECONDS_PER_YEAR = 31536000000L;
+
+    public Person(String name,Calendar birthday) {
+        this.name = name;
+        this.birthday = birthday; 
     }
     
+    public int getAge(GregorianCalendar a) {
+        return(int)(milliSecondsToYear(a.getTimeInMillis()-this.birthday.getTimeInMillis()));
+    }
     
+    private long milliSecondsToYear(long milles) {
+        return milles/MILLISECONDS_PER_YEAR;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public Calendar getBirthday(){
+        return birthday;
+    }
 }
